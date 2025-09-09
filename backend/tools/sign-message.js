7d1bf599705320e9029e0da3e584124b5aef3104
Ronak Maniya <ronakmaniya2005@gmail.com>
2025-09-09 15:54:05 +0530

    Comments added for developer convience

--- backend/tools/sign-message.js
+++ backend/tools/sign-message.js
@@ -1,6 +1,7 @@
 // backend/tools/sign-message.js
 // Usage: node tools/sign-message.js <privateKey> <nonce>
 const { Wallet } = require("ethers");
+// const { verifyMessage } = require("ethers");
 
 async function main() {
   const [, , privateKey, nonce] = process.argv;
@@ -9,8 +10,12 @@ async function main() {
     process.exit(1);
   }
   const wallet = new Wallet(privateKey);
+  // console.log(wallet);
   const signature = await wallet.signMessage(nonce);
   console.log(signature);
+  // const recovered = verifyMessage(nonce, signature).toLowerCase();
+  // console.log(recovered);
+  // console.log(recovered == wallet.address.toLowerCase());
 }
 
 main();
